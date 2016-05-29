5f527305a07b302a705e33e84595fd9e4d32802a
alehat <theassd1337@yandex.ru>
2016-05-29 23:06:27 +0300

    add new status page

--- searchres/urls.py
+++ searchres/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url(r'^$', views.home_page),
     url(r'^results/?$', views.search_result),
     url(r'^index/?$', views.indexing_url),
-    url(r'^url/view/?$', views.editing_url),
+    url(r'^view/?$', views.editing_url),
     url(r'^status/?$', views.status),
     url(r'^about/?$', views.about),
 ]
