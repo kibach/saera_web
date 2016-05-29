86430200d1f4fadab684581114597e12f01c86a5
alehat <theassd1337@yandex.ru>
2016-05-29 19:41:29 +0300

    add indexing url page

--- searchres/urls.py
+++ searchres/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home_page),
     url(r'^results/?$', views.search_result),
-    url(r'^url/index/?$', views.indexing_url),
+    url(r'^index/?$', views.indexing_url),
     url(r'^url/view/?$', views.editing_url),
     url(r'^status/?$', views.status),
     url(r'^about/?$', views.about),
