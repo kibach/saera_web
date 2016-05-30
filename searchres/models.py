7837513350120cddf7de11ae779cc79bb5d0d99e
wget <theassd@yandex.com>
2016-05-30 20:32:36 +0300

    Models added

--- searchres/models.py
+++ searchres/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    url = models.CharField(max_length=2000, db_index=True)
+    domain = models.CharField(max_length=500, db_index=True)
+    title = models.CharField(max_length=1000)
+    language = models.CharField(max_length=20)
+    encoding = models.CharField(max_length=20)
+    contents = models.TextField()
+    plaintext = models.TextField()
+    indexed_at = models.DateTimeField()
+
+
+class Stem(models.Model):
+    stem = models.CharField(max_length=500, unique=True)
+
+
+class DocumentMap(models.Model):
+    A = models.IntegerField(db_index=True)
+    B = models.IntegerField(db_index=True)
+
+
+class DocumentStemMap(models.Model):
+    doc = models.ForeignKey(Document, db_index=True)
+    stem = models.ForeignKey(Stem, db_index=True)
+    count = models.IntegerField()
+    type = models.IntegerField()
--- searchres/views.py
+++ searchres/views.py
@@ -1,7 +1,44 @@
 from django.shortcuts import render
+from langdetect import detect
+import snowballstemmer
+
+
+LANGUAGES = {
+    'en': 'English',
+    'de': 'German',
+    'fr': 'French',
+    'ru': 'Russian',
+    'es': 'Spanish',
+    'pt': 'Portuguese',
+    'it': 'Italian',
+    'hr': 'Croatian',
+    'mk': 'Macedonian',
+    'ar': 'Arabic',
+    'fi': 'Finnish',
+    'no': 'Norwegian',
+    'tr': 'Turkish',
+    'pl': 'Polish',
+    'sv': 'Swedish',
+    'uk': 'Ukrainian',
+    'ja': 'Japanese',
+    'cs': 'Czech',
+    'zh': 'Chinese',
+    'kr': 'Korean',
+}
 
 
 def search_result(request):
+    query = request.POST.get('query')
+    q_words = query.split()
+    stemmed_words = []
+    for word in q_words:
+        lng = detect(word)
+        if lng in LANGUAGES:
+            lng = LANGUAGES[lng]
+            stemmed_words.append(snowballstemmer.stemmer(lng).stemWord(word))
+        else:
+            stemmed_words.append(word)
+
     return render(request, 'searchres/search_result.html', {})
 
 
